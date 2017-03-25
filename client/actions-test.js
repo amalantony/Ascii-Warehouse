77e80f590f0b5f52a50c1fa281734d558baee5a8
Amal Antony <amalantony06@gmail.com>
2017-03-25 13:22:16 +0530

    Added files missed in the previous commit & custom css

---
+++ client/actions-test.js
@@ -0,0 +1,45 @@
+import expect from 'expect';
+
+import {
+    loadProducts, 
+    fetchProductsRequest, 
+    fetchProductsSuccess, 
+    fetchProductsFailure,
+    changeProductsFilter,
+    fetchAdRequest,
+    fetchAdFailure,
+    fetchAdSuccess } from './actions.js';
+
+expect(loadProducts()).toEqual({
+    type: 'LOAD_PRODUCTS'
+});
+
+expect(fetchProductsRequest()).toEqual({
+    type: 'FETCH_PRODUCTS_REQUEST'
+});
+
+expect(fetchProductsSuccess()).toEqual({
+    type: 'FETCH_PRODUCTS_SUCCESS'
+});
+
+expect(fetchProductsFailure()).toEqual({
+    type: 'FETCH_PRODUCTS_FAILURE'
+});
+
+expect(changeProductsFilter()).toEqual({
+    type: 'CHANGE_PRODUCTS_FILTER'
+});
+
+expect(fetchAdRequest()).toEqual({
+    type: 'FETCH_AD_REQUEST'
+});
+
+expect(fetchAdFailure()).toEqual({
+    type: 'FETCH_AD_FAILURE'
+});
+
+expect(fetchAdSuccess()).toEqual({
+    type: 'FETCH_AD_SUCCESS'
+});
+
+console.log("Actions tests passed!");
---
+++ client/actions.js
@@ -0,0 +1,55 @@
+export const LOAD_PRODUCTS = 'LOAD_PRODUCTS';
+export const loadProducts = () => {
+    return {
+        type: 'LOAD_PRODUCTS'
+    };
+};
+
+export const FETCH_PRODUCTS_REQUEST = 'FETCH_PRODUCTS_REQUEST';
+export const fetchProductsRequest = () => {
+    return {
+        type: FETCH_PRODUCTS_REQUEST
+    };
+};
+
+export const FETCH_PRODUCTS_FAILURE = 'FETCH_PRODUCTS_FAILURE';
+export const fetchProductsFailure = () => {
+    return {
+        type: FETCH_PRODUCTS_FAILURE
+    };
+};
+
+export const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS';
+export const fetchProductsSuccess = () => {
+    return {
+        type: FETCH_PRODUCTS_SUCCESS
+    };
+};
+
+export const CHANGE_PRODUCTS_FILTER = 'CHANGE_PRODUCTS_FILTER';
+export const changeProductsFilter = () => {
+    return {
+        type: CHANGE_PRODUCTS_FILTER
+    };
+};
+
+export const FETCH_AD_REQUEST = 'FETCH_AD_REQUEST';
+export const fetchAdRequest = () => {
+    return {
+        type: FETCH_AD_REQUEST
+    };
+};
+
+export const FETCH_AD_FAILURE = 'FETCH_AD_FAILURE';
+export const fetchAdFailure = () => {
+    return {
+        type: FETCH_AD_FAILURE
+    };
+};
+
+export const FETCH_AD_SUCCESS = 'FETCH_AD_SUCCESS';
+export const fetchAdSuccess = () => {
+    return {
+        type: FETCH_AD_SUCCESS
+    };
+};
