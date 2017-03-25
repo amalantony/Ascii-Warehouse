71f33b67b8176b7d643f6765c31b1caf452b9fd7
Amal Antony <amalantony06@gmail.com>
2017-03-25 13:21:34 +0530

    Added action creators & action creator tests

--- DesignDocument.md
+++ DesignDocument.md
@@ -1,6 +1,6 @@
 Design Goals: 
 
-- Keep the app as functional & modular as possible (React components must be functional as far as possible)
+- Keep the app as functional & modular as possible (React components must be functional as far as possible) - what are the tradeoffs
 - Using an external library to generate the scoll to bottom event (to keep code clean)
 
 
@@ -19,20 +19,40 @@ Component List:
 
 State:
 {
-    advertisement: {
-        prevousAd: //id of the last Advertisement,
+    products: {
+        list: [], //  list of all the products,
+        prefetch: [], // set of all prefetched results
+        runningCount:  //  count in multiples of 20 to display the Ad
+        isFetching: true or false // used by UI to toggle loading or not,
+        lastUpdated: // timestamp
+        advertisement: {
+            previousAd: // id of the last shown Ad,
+            isFetching: // status of the ad fetch
+        }
     },
-    products:{ 
-        //  list of all the products,
-        //  count in multiples of 20 to display the Ad
-    },
-    fetchStatus: FETCH_ACIVE | FETCH_PAUSED | FETCH_SUCCESS | FETCH_ERROR
-    activeFilter: SIZE | PRICE | ID,
-    catalogStatus: ACTIVE | ENDED
+    activeFilter: SIZE | PRICE | ID
 }
+
+Actions: 
+    LOAD_PRODUCTS (?) -> Load products on page load & scoll
+    FETCH_PRODUCTS_REQUEST
+    FETCH_PRODUCTS_FAILURE
+    FETCH_PRODUCTS_SUCCESS
+    CHANGE_PRODUCT_FILTER {filter: 'SIZE'}
+
+    PREFETCH_PRODUCTS_DATA
+
+    FETCH_AD_REQUEST
+    FETCH_AD_FAILURE
+    FETCH_AD_SUCCESS
 
 Points to consider -> 
     - What about conflicts arising out of an ongoing optimistic fetch vs when a user scolls to bottom before the fetch is satisified?
+            Look at the isFetching flag, if it's True, cancel any new FETCH_PRODUCTS_REQUEST
+            If prefetch[] is not empty, cancel any new FETCH_PRODUCTS_REQUEST
+
     - How & who determines if the catalog has ended & if further fetches must be disabled? 
     - The fetch must probably not permanently be disabled though, since the server could have new products added in the meantime
     - Who decides the advertisement placement & which state variable will keep track of it? (probably as part of the props that the ProductGrid will pass onto it's child Component, the Product Row)
+
+    Advanced Fetch -> When FETCH_PAUSED && Prefetched results are empty
--- client/client.js
+++ client/client.js
@@ -1,34 +1,41 @@
+import './actions-test.js';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 const TitleBar = () => {
     return (
-        <div className = "section hero">
-            <div className = "container">
-                <div className = "row">
-                    <h4 class = "hero-heading"> ASCII WAREHOUSE </h4>
+        <div className="section hero">
+            <div className="container">
+                <div className="row">
+                    <h4 className="hero-heading"> ASCII WAREHOUSE </h4>
                 </div>
             </div>
         </div>
     );
 };
 
+/* 
+*   Accepts the product image, Price & Size as props and renders the product 
+*/
 const Product = () => {
-    return (<div className="one-third column">
-        Product
+    return (<div className="one-third column listing-item">
     </div>);
 };
 
 const Advertisement = () => {
-    return (<div className="one-third column">
+    return (<div className="one-third column listing-item">
         Product
     </div>);
 };
 
+/*
+*   Take 3 DisplayItems (Products/Advertisements) and render them
+*/
 const ProductRow = () => {
     return (
-        <div className = "container">
-            <div className = "row">
+        <div className="container">
+            <div className="row">
                 <Product/>
                 <Product/>
                 <Product/>
@@ -39,9 +46,11 @@ const ProductRow = () => {
 
 const ProductGrid = () => {
     return (
-       <div> 
+       <div>
             <TitleBar/>
             <SortFilter/>
+            <ProductRow/>
+            <ProductRow/>
             <ProductRow/>
         </div>
     );
@@ -49,11 +58,11 @@ const ProductGrid = () => {
 
 const SortFilter = () => {
     return (
-        <div className = "container">
+        <div className="container">
             <div className="row">
-                <div class="four columns">
-                    <label for="drop-down-filter"> Sort By: </label>
-                    <select class="u-full-width" id="drop-down-filter">
+                <div className="four columns">
+                    <label htmlFor="drop-down-filter"> Sort By: </label>
+                    <select className="u-full-width" id="drop-down-filter">
                         <option value="price">Price</option>
                         <option value="size">Size</option>
                         <option value="id">Id</option>
